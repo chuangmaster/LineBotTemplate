e72b7643f7fcf70d2947f2777d52acfa27a01271
chuangmaster <chuang.master@gmail.com>
2018-12-30 01:14:02 +0800

    加入Line Bot SDK；加入API Controller；加入Liff畫面

---
+++ LineBotTemplate/Controllers/LiffController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LineBotTemplate.Controllers
+{
+    public class LiffController : Controller
+    {
+        // GET: Liff
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
